b9a2505325e7638f0795819866ca523caaa49378
Aiden Taylor <ataylor3008@gmail.com>
2025-09-03 11:46:10 +0100

    Navbar start, Login/Signup files created

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='h-18 bg-black rounded-b-md flex items-center'>
+        <h1 className="font-bold text-white ml-5 w-64 text-2xl text-center">The Gateway</h1>
+        {/*Navbar Buttons */}
+        <div></div>
+        {/*Login/Signup Logic */}
+        <div></div>
+    </div>
+  )
+}
